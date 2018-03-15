e386445e10003a9b11f4ae497f3ac6607c03c50f
JCharlton1 <jcharlton-student@nfcs.k12.pa.us>
2018-03-15 19:19:38 -0400

    Update SocialMedia.java

--- SocialMedia.java
+++ SocialMedia.java
@@ -17,6 +17,12 @@ public class SocialMedia{
         AddResponses.add("Add");
         AddResponses.add("post");
         AddResponses.add("add");
+        
+        ArrayList<String> RemoveResponses = new ArrayList<String>();
+        RemoveResponses.add("Delete");
+        RemoveResponses.add("Remove");
+        RemoveResponses.add("delete");
+        RemoveResponses.add("remove");
 
         ArrayList<String> PrintResponses = new ArrayList<String>();
         PrintResponses.add("Show");
@@ -32,13 +38,24 @@ public class SocialMedia{
         else if(PrintResponses.indexOf(response) > -1){
           printPost(posts);
         }
+        
+        else if(RemoveResponses.indexOf(response) > - 1){
+            printPost(posts);
+            removePost();
+        }
     }
 
     public  void addPost(){        
            System.out.println("What would you like to post?");
            response = sc.getLine();
            posts.add(response);
-    }        
+    }  
+    
+    public void removePost(){
+        System.out.print("Which post do you want to remove?");
+        int x = sc.getInt();
+        posts.remove(posts.get(x - 1));
+    }
 
     public  void printPost(ArrayList<String> list){
 
