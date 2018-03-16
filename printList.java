8c9cfa2e1c434a589a62986a7e6db71c9b8c2ca3
Matthew Biegnewald <mbiegenwald-student@nfcs.k12.pa.us>
2018-03-16 12:54:52 -0400

    Update printList.java

--- printList.java
+++ printList.java
@@ -8,8 +8,11 @@ import java.util.ArrayList;
 public class printList
 {
     public static void printArray(ArrayList<String> list){
+        //print each individual object in the array on a seperate line.
         for(int i = 0; i < list.size(); i++){
+            //show each post's number starting at 1 instead of 0 (length 0 in the array).
             int num = i + 1;
+            //prints out the number position in the list from the variable above plus the post itself.
             System.out.println(num + ". " + list.get(i));
         }
     }
